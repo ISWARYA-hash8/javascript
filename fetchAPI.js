18be5f393e88d65c61bc1e33056659cd356576f9
ISWARYA835 <iswarya83p@gmail.com>
2025-03-15 15:58:04 +0530

    initial changes

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,27 +1,33 @@
-fetch("https://fakestoreapi.com/users/abcd")
-.then( (response)=>{
-    if(!response.ok){
-        throw new Error("Data Not Found") //if the response is false this then block directly throws error to catch block
+// fetch("https://fakestoreapi.com/users/")
+// .then( (response)=>{
+//     if(!response.ok){
+//         throw new Error("Data Not Found") //if the response is false this then block directly throws error to catch block
 
-    }
-    console.log(response.ok)
-    return (response.json()) //used to convert the backend data into nora]mal javascript form 
+//     }
+//     console.log(response.ok)
+//     return (response.json()) //used to convert the backend data into nora]mal javascript form 
 
-})
-.then ( ( data ) =>{ console.log( data ); //coverting the promise into the object 
+// })
+// .then ( ( data ) =>{ console.log( data ); //coverting the promise into the object 
 
-})
-.catch( (error)=>{
-    console.log(error.message)
-})
+// })
+// .catch( (error)=>{
+//     console.log(error.message)
+// })
 
 
 async function fetchData(){
     try{
-        
+       let response =await fetch("https://fakestoreapi.com/users")
+       if(!response){
+        throw new Error("Data not found")
+       }
+       let data = await response.json()
+       console.log( data )
 
     }
     catch(error){
-
+console.log("error.message")
     }
 }
+fetchData()
