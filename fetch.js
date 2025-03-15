46b38f255a9c455a2704ef09839e458766d048e7
ISWARYA835 <iswarya83p@gmail.com>
2025-03-15 09:15:54 +0530

    initial changes

--- fetch.js
+++ fetch.js
@@ -1,13 +1,26 @@
 function fetchProfile( successcallBack ,errorcallBack ){
-    let dataRecieved = true;
+    let dataRecieved = false;
     if(dataRecieved){
-        successcallBack()
+        successcallBack("Data Received ")
     }
     else{
-        errorcallBack()
+        errorcallBack("Data Not Recieved")
     }
 }
 fetchProfile( 
-    ()=>{},
-    ()=>{}
+    (message)=>{
+        console.log( message ); 
+        fetchProfile( 
+            (nextMessage) => { //call back hell because we are using nested call back function 
+                console.log("Next Message: "+ nextMessage);
+
+            },  
+            (nextError) =>{ 
+                 console.log("Next Error:" +nextError);
+            }
+        )
+    },
+    (error )=>{ 
+        console.log( error )
+    }
 )
